728652bc1b94c323f5ce99fe8c51426200bf01b4
PiatrouskiIM <petrovskiim97@gmail.com>
2023-04-25 16:06:06 +0300

    include tqdm module and ffmpeg preprocessing

--- run.py
+++ run.py
@@ -5,8 +5,9 @@ import time
 import os
 import math
 import cv2
-
-#  ffmpeg -i input.avi -vf "pad=width=640:height=480:x=0:y=0:color=black" output.avi
+import tempfile
+from utils.video import pad_video_to_be_multiple_of_factor, crop_video
+from tqdm import tqdm
 
 
 def preprocess(x: np.ndarray):
@@ -17,11 +18,16 @@ def preprocess(x: np.ndarray):
     w_n = int(4 * np.ceil(x.shape[3] / 4))
     shape = x.shape
     shape = shape[:-2] + (h_n, w_n)
-    # shape[-1] = w_n
-    # shape[-2] = h_n
     imgs_temp = np.zeros(shape)
     imgs_temp[:, :, 0:x.shape[-2], 0:x.shape[-1]] = x
     return imgs_temp  # (reshape) B x H x W x C -> B x C x H x W
+
+
+def fast_preprocess(x: np.ndarray):
+    x = x[..., ::-1]  # (reorder color channels) BGR -> RGB
+    x = x / 255  # (range change) [0, 255] -> [0, 1]
+    x = x.transpose(0, 3, 1, 2)
+    return x
 
 
 def postprocessing(x: np.ndarray):
@@ -34,57 +40,53 @@ def postprocessing(x: np.ndarray):
 
 @torch.no_grad()
 def run_on_video(input_path, output_path, model, batch_size=1, device="cuda") -> None:
-    point_time = time.time()
     assert os.path.isfile(input_path), f"{input_path} not found."
 
-    reader = cv2.VideoCapture(input_path)
-    fps = int(math.ceil(reader.get(cv2.CAP_PROP_FPS)))
-    size = (int(reader.get(cv2.CAP_PROP_FRAME_WIDTH)), int(reader.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    r = model.upscale_factor
-    size = (r * int(r * np.ceil(size[0] / r)), r * int(r * np.ceil(size[1] / r)))
+    model.to(args.device)
+    model.eval()
+    with tempfile.TemporaryDirectory() as directory:
+        temp_input_path, temp_output_path = os.path.join(directory, "input.mp4"), os.path.join(directory, "output.mp4")
 
-    writer = cv2.VideoWriter(filename=output_path,
-                             fourcc=cv2.VideoWriter_fourcc(*"mp4v"),
-                             fps=fps * 2,
-                             frameSize=size,
-                             isColor=True)
+        pad_video_to_be_multiple_of_factor(input_path, temp_input_path, factor=4)
+        reader = cv2.VideoCapture(temp_input_path)
+        fps = int(math.ceil(reader.get(cv2.CAP_PROP_FPS)))
+        w, h = (int(reader.get(cv2.CAP_PROP_FRAME_WIDTH)), int(reader.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        r = model.upscale_factor
+        size = (r * int(r * np.ceil(w / r)), r * int(r * np.ceil(h / r)))
 
-    frame_count = int(reader.get(cv2.CAP_PROP_FRAME_COUNT))
-    processing_stats = [0]
+        writer = cv2.VideoWriter(filename=temp_output_path,
+                                 fourcc=cv2.VideoWriter_fourcc(*"mp4v"),
+                                 fps=fps * 2,
+                                 frameSize=size,
+                                 isColor=True)
 
-    frame_no = 0
-    status, frame = reader.read()
-    if not status:
-        return
-    # writer.write(frame)
-    frames = [frame]
-    i = 0
-    while status:# and i<220:
-        i+=1
-        batch_time = time.time()
-        for _ in range(batch_size):
-            frame_no += 1
-            status, frame = reader.read()
-            if not status:
-                break
-            frames.append(frame)
-        frames = frames[-batch_size - 1:]
-        x = preprocess(np.stack(frames))[:, None]
-        x = torch.Tensor(x).to(device)
-        predicted_patch = model(x)#.cpu().detach().numpy()
-        for frame in predicted_patch[1:]:
-            x = frame.cpu().detach().numpy()
-            x = postprocessing(x)[0]
-            writer.write(x)
+        frame_count = int(reader.get(cv2.CAP_PROP_FRAME_COUNT))
+        processing_bar = tqdm(total=frame_count)
 
-        step_time = time.time() - batch_time
-        processing_stats.append(step_time)
-        processing_stats = processing_stats[-10:]
-        print(f"total time: "
-              f"{time.time() - point_time:0.2f}/~{frame_count / batch_size * np.mean(processing_stats):0.2f} sec., "
-              f"total frames: {str(frame_no).zfill(3)}/{str(frame_count).zfill(3)} frames.")
-    reader.release(), writer.release()
-    print(f"pass {time.time() - point_time:0.2f} sec.")
+        frame_no = 0
+        status, frame = reader.read()
+        if not status:
+            return
+        # writer.write(frame)
+        frames = [frame]
+        while status:
+            for _ in range(batch_size):
+                frame_no += 1
+                status, frame = reader.read()
+                if not status:
+                    break
+                frames.append(frame)
+            frames = frames[-batch_size - 1:]
+            x = fast_preprocess(np.stack(frames))[:, None]
+            x = torch.Tensor(x).to(device)
+            predicted_patch = model(x)
+            for frame in predicted_patch[1:]:
+                x = frame.cpu().detach().numpy()
+                x = postprocessing(x)[0]
+                writer.write(x)
+            processing_bar.update(batch_size)
+        crop_video(temp_output_path, output_path, [0, 0, w, h])
+        reader.release(), writer.release(), processing_bar.close()
 
 
 if __name__ == "__main__":
@@ -97,11 +99,15 @@ if __name__ == "__main__":
     parser.add_argument("--batch", default=1, type=int, help="device.")
     args = parser.parse_args()
 
-    from modules import ZSM
+    from modules import zsm, ZSM_Weights
 
-    a = ZSM(3, 3)
-    a.load_state_dict(torch.load("checkpoint/zsm.pt"))
-    a.to(args.device)
-    a.eval()
+
+    class PlaceHolder:
+
+        def get_state_dict(self, *args, **kwargs):
+            return torch.load("checkpoint/zsm.pt")
+
+
+    a = zsm(weights=PlaceHolder())
 
     run_on_video(args.input, args.output, a, batch_size=args.batch, device=args.device)
